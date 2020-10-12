6b3be13c60db3c3f1d409a5cedd3839ddbb60e53
csruiliu <rui.liu@proton.me>
2020-10-12 15:02:24 -0500

    update dnn models

--- models/model_mobilenet.py
+++ models/model_mobilenet.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import tensorflow.contrib as tc
+from utils_model_func import activation_function
 
 
 # MobileNetV2
@@ -17,22 +18,25 @@ class mobilenet(object):
         self.learning_rate = learning_rate
         self.activation = activation
         self.batch_padding = batch_padding
+
         self.model_logit = None
         self.train_op = None
         self.eval_op = None
-        self.is_training = True
+
         self.num_conv_layer = 0
+        self.num_pool_layer = 0
+        self.num_residual_layer = 0
 
     def _res_block(self, input, expansion_ratio, output_dim, stride, is_train, block_name, bias=False, shortcut=True):
         with tf.variable_scope(block_name):
             bottleneck_dim = round(expansion_ratio * input.get_shape().as_list()[-1])
             net = self._conv_1x1(input, bottleneck_dim, name='pw', bias=bias)
             net = self._batch_norm(net, train=is_train, name='pw_bn')
-            net = tf.nn.relu6(net, 'relu6')
+            net = activation_function(net, self.activation)
 
             net = self._dwise_conv(net, strides=[1, stride, stride, 1], name='dw', bias=bias)
             net = self._batch_norm(net, train=is_train, name='dw_bn')
-            net = tf.nn.relu6(net, 'relu6')
+            net = activation_function(net, self.activation)
 
             net = self._conv_1x1(net, output_dim, name='pw_linear', bias=bias)
             net = self._batch_norm(net, train=is_train, name='pw_linear_bn')
@@ -43,105 +47,109 @@ class mobilenet(object):
                 if in_dim != output_dim:
                     ins = self._conv_1x1(input, output_dim, name='ex_dim')
                     net = ins + net
+                    self.add_layer_num('residual', 1)
                 else:
                     net = input + net
+                    self.add_layer_num('residual', 1)
 
         return net
 
-    def _conv2d_block(self, input, out_dim, kernel_size, strides_size, is_train, block_name):
+    def _conv2d_block(self, x_init, out_dim, kernel_size, strides_size, is_train, block_name):
         with tf.variable_scope(block_name):
-            block = self._conv2d(input, out_dim, kernel_size, kernel_size, strides_size, strides_size)
+            block = self._conv2d(x_init, out_dim, kernel_size, kernel_size, strides_size, strides_size)
             block = self._batch_norm(block, train=is_train, name='bn')
-            block = tf.nn.relu6(block, 'relu6')
+            block = activation_function(block, self.activation)
         return block
 
-    def _pwise_block(self, input, output_dim, is_train, block_name, bias=False):
+    def _pwise_block(self, x_init, output_dim, is_train, block_name, bias=False):
         with tf.variable_scope(block_name):
-            out = self._conv_1x1(input, output_dim, bias=bias, name='pwb')
+            out = self._conv_1x1(x_init, output_dim, bias=bias, name='pwb')
             out = self._batch_norm(out, train=is_train, name='bn')
-            block = tf.nn.relu6(out, 'relu6')
+            block = activation_function(out, self.activation)
         return block
 
-    def _conv2d(self, input, output_dim, kernel_height, kernel_width, strides_h, strides_w, stddev=0.02, bias=False,
+    def _conv2d(self, x_init, output_dim, kernel_height, kernel_width, strides_h, strides_w, stddev=0.02, bias=False,
                 name='conv2d'):
         weight_decay = 1e-4
         with tf.variable_scope(name):
-            w = tf.get_variable('w', [kernel_height, kernel_width, input.get_shape()[-1], output_dim],
+            w = tf.get_variable('w', [kernel_height, kernel_width, x_init.get_shape()[-1], output_dim],
                                 regularizer=tc.layers.l2_regularizer(weight_decay),
                                 initializer=tf.truncated_normal_initializer(stddev=stddev))
-            conv = tf.nn.conv2d(input, w, strides=[1, strides_h, strides_w, 1], padding='SAME')
-            self.num_conv_layer += 1
+            conv = tf.nn.conv2d(x_init, w, strides=[1, strides_h, strides_w, 1], padding='SAME')
+            self.add_layer_num('conv', 1)
             if bias:
                 biases = tf.get_variable('bias', [output_dim], initializer=tf.constant_initializer(0.0))
                 conv = tf.nn.bias_add(conv, biases)
         return conv
 
-    def _dwise_conv(self, input, kernel_height=3, kernel_width=3, channel_multiplier=1, strides=[1, 1, 1, 1],
+    def _dwise_conv(self, x_init, kernel_height=3, kernel_width=3, channel_multiplier=1, strides=[1, 1, 1, 1],
                     padding='SAME', stddev=0.02, name='dwise_conv', bias=False):
         weight_decay = 1e-4
         with tf.variable_scope(name):
-            in_channel = input.get_shape().as_list()[-1]
+            in_channel = x_init.get_shape().as_list()[-1]
             w = tf.get_variable('w', [kernel_height, kernel_width, in_channel, channel_multiplier],
                                 regularizer=tc.layers.l2_regularizer(weight_decay),
                                 initializer=tf.truncated_normal_initializer(stddev=stddev))
-            conv = tf.nn.depthwise_conv2d(input, w, strides, padding, rate=None, name=None, data_format=None)
-            self.num_conv_layer += 1
+            conv = tf.nn.depthwise_conv2d(x_init, w, strides, padding, rate=None, name=None, data_format=None)
+            self.add_layer_num('conv', 1)
             if bias:
                 biases = tf.get_variable('bias', [in_channel * channel_multiplier],
                                          initializer=tf.constant_initializer(0.0))
                 conv = tf.nn.bias_add(conv, biases)
-            return conv
+        return conv
 
-    def _batch_norm(self, x, momentum=0.9, epsilon=1e-5, train=True, name='bn'):
-        return tf.layers.batch_normalization(x, momentum=momentum, epsilon=epsilon, scale=True, training=train,
+    def _batch_norm(self, x_init, momentum=0.9, epsilon=1e-5, train=True, name='bn'):
+        return tf.layers.batch_normalization(x_init, momentum=momentum, epsilon=epsilon, scale=True, training=train,
                                              name=name)
 
-    def _conv_1x1(self, input, output_dim, name, bias=False):
+    def _conv_1x1(self, x_init, output_dim, name, bias=False):
         with tf.variable_scope(name):
-            net = self._conv2d(input, output_dim, 1, 1, 1, 1, stddev=0.02, name=name, bias=bias)
-            self.num_conv_layer += 1
+            net = self._conv2d(x_init, output_dim, 1, 1, 1, 1, stddev=0.02, name=name, bias=bias)
+            self.add_layer_num('conv', 1)
         return net
 
     def _global_avg(self, x):
         with tf.name_scope('global_avg'):
             net = tf.layers.average_pooling2d(x, x.get_shape()[1:-1], 1)
+            self.add_layer_num('pool', 1)
         return net
 
-    def build(self, input):
+    def build(self, input_features, is_training):
         exp = 6
         if self.batch_padding:
-            input = input[0:self.batch_size, :, :, :]
+            train_input = input_features[0:self.batch_size, :, :, :]
+        else:
+            train_input = input_features
 
         with tf.variable_scope(self.net_name + '_instance'):
-            net = self._conv2d_block(input, 32, 3, 2, self.is_training, 'conv1_1')
-            net = self._res_block(net, 1, 16, 1, self.is_training, block_name='res2_1')
-            net = self._res_block(net, exp, 24, 2, self.is_training, block_name='res3_1')  # size/4
-            net = self._res_block(net, exp, 24, 1, self.is_training, block_name='res3_2')
+            net = self._conv2d_block(train_input, 32, 3, 2, is_training, 'conv1_1')
+            net = self._res_block(net, 1, 16, 1, is_training, block_name='res2_1')
+            net = self._res_block(net, exp, 24, 2, is_training, block_name='res3_1')  # size/4
+            net = self._res_block(net, exp, 24, 1, is_training, block_name='res3_2')
 
-            net = self._res_block(net, exp, 32, 2, self.is_training, block_name='res4_1')  # size/8
-            net = self._res_block(net, exp, 32, 1, self.is_training, block_name='res4_2')
-            net = self._res_block(net, exp, 32, 1, self.is_training, block_name='res4_3')
+            net = self._res_block(net, exp, 32, 2, is_training, block_name='res4_1')  # size/8
+            net = self._res_block(net, exp, 32, 1, is_training, block_name='res4_2')
+            net = self._res_block(net, exp, 32, 1, is_training, block_name='res4_3')
 
-            net = self._res_block(net, exp, 64, 2, self.is_training, block_name='res5_1')
-            net = self._res_block(net, exp, 64, 1, self.is_training, block_name='res5_2')
-            net = self._res_block(net, exp, 64, 1, self.is_training, block_name='res5_3')
-            net = self._res_block(net, exp, 64, 1, self.is_training, block_name='res5_4')
+            net = self._res_block(net, exp, 64, 2, is_training, block_name='res5_1')
+            net = self._res_block(net, exp, 64, 1, is_training, block_name='res5_2')
+            net = self._res_block(net, exp, 64, 1, is_training, block_name='res5_3')
+            net = self._res_block(net, exp, 64, 1, is_training, block_name='res5_4')
 
-            net = self._res_block(net, exp, 96, 1, self.is_training, block_name='res6_1')  # size/16
-            net = self._res_block(net, exp, 96, 1, self.is_training, block_name='res6_2')
-            net = self._res_block(net, exp, 96, 1, self.is_training, block_name='res6_3')
+            net = self._res_block(net, exp, 96, 1, is_training, block_name='res6_1')  # size/16
+            net = self._res_block(net, exp, 96, 1, is_training, block_name='res6_2')
+            net = self._res_block(net, exp, 96, 1, is_training, block_name='res6_3')
 
-            net = self._res_block(net, exp, 160, 2, self.is_training, block_name='res7_1')  # size/32
-            net = self._res_block(net, exp, 160, 1, self.is_training, block_name='res7_2')
-            net = self._res_block(net, exp, 160, 1, self.is_training, block_name='res7_3')
+            net = self._res_block(net, exp, 160, 2, is_training, block_name='res7_1')  # size/32
+            net = self._res_block(net, exp, 160, 1, is_training, block_name='res7_2')
+            net = self._res_block(net, exp, 160, 1, is_training, block_name='res7_3')
 
-            net = self._res_block(net, exp, 320, 1, self.is_training, block_name='res8_1', shortcut=False)
-            net = self._pwise_block(net, 1280, self.is_training, block_name='conv9_1')
+            net = self._res_block(net, exp, 320, 1, is_training, block_name='res8_1', shortcut=False)
+            net = self._pwise_block(net, 1280, is_training, block_name='conv9_1')
             net = self._global_avg(net)
 
             self.model_logit = tc.layers.flatten(self._conv_1x1(net, self.num_classes, name='logits'))
 
-        print('number of conv layer:', self.num_conv_layer)
         return self.model_logit
 
     def train(self, logits, labels):
@@ -172,3 +180,20 @@ class mobilenet(object):
             self.eval_op = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
         return self.eval_op
+
+    def add_layer_num(self, layer_type, layer_num):
+        if layer_type == 'pool':
+            self.num_pool_layer += layer_num
+        elif layer_type == 'conv':
+            self.num_conv_layer += layer_num
+        elif layer_type == 'residual':
+            self.num_residual_layer += layer_num
+
+    def get_layer_info(self):
+        return self.num_conv_layer, self.num_pool_layer, self.num_residual_layer
+
+    def print_model_info(self):
+        print('=====================================================================')
+        print('number of conv layer: {}, number of pooling layer: {}, number of residual layer: {}'
+              .format(self.num_conv_layer, self.num_pool_layer, self.num_residual_layer))
+        print('=====================================================================')
