109988c2975af6fead94501ed3e016064297c1b8
csruiliu <rui.liu@proton.me>
2020-10-12 21:42:25 -0500

    add config path and parameter

--- config/config_parameter.py
+++ config/config_parameter.py
@@ -1,7 +1,7 @@
 import yaml
 
-with open("config.yml", 'r') as ymlfile:
-    cfg = yaml.load(ymlfile)
+with open("config_parameter.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
 ##########################################
 # Hyperparameters for input data
@@ -60,23 +60,3 @@ pack_measure_step = hyperparams_pack_cfg['measure_step']
 pack_same_input = hyperparams_pack_cfg['same_input']
 pack_use_tb_timeline = hyperparams_pack_cfg['use_tb_timeline']
 
-##########################################
-# Path
-##########################################
-
-#path_cfg = cfg['local_path']
-#path_cfg = cfg['roscoe_path']
-path_cfg = cfg['hp_path']
-
-mnist_train_img_path = path_cfg['mnist_train_img_path']
-mnist_train_label_path = path_cfg['mnist_train_label_path']
-mnist_t10k_img_path = path_cfg['mnist_t10k_img_path']
-mnist_t10k_label_path = path_cfg['mnist_t10k_label_path']
-cifar_10_path = path_cfg['cifar_10_path']
-imagenet_t1k_img_path = path_cfg['imagenet_t1k_img_path']
-imagenet_t1k_label_path = path_cfg['imagenet_t1k_label_path']
-imagenet_t1k_bin_path = path_cfg['imagenet_t1k_bin_path']
-imagenet_t10k_img_path = path_cfg['imagenet_t10k_img_path']
-imagenet_t10k_label_path = path_cfg['imagenet_t10k_label_path']
-imagenet_t10k_bin_path = path_cfg['imagenet_t10k_bin_path']
-profile_path = path_cfg['profile_path']
---
+++ config/config_path.py
@@ -0,0 +1,22 @@
+import yaml
+
+with open("config_path.yml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
+#path_cfg = cfg['local_path']
+#path_cfg = cfg['roscoe_path']
+path_cfg = cfg['hp_path']
+
+imagenet_t50k_img_raw_path = path_cfg['dataset_imagenet_t50k_img_raw']
+imagenet_t50k_label_path = path_cfg['dataset_imagenet_t50k_label']
+imagenet_t10k_img_raw_path = path_cfg['dataset_imagenet_t10k_img_raw']
+imagenet_t10k_img_bin_path = path_cfg['dataset_imagenet_t10k_img_bin']
+imagenet_t10k_label_path = path_cfg['dataset_imagenet_t10k_label']
+imagenet_t1k_img_raw_path = path_cfg['dataset_imagenet_t1k_img_raw']
+imagenet_t1k_img_bin_path = path_cfg['dataset_imagenet_t1k_img_bin']
+imagenet_t1k_label_path = path_cfg['dataset_imagenet_t1k_label']
+mnist_train_img_path = path_cfg['dataset_mnist_train_img']
+mnist_train_label_path = path_cfg['dataset_mnist_train_label']
+mnist_test_10k_img_path = path_cfg['dataset_mnist_t10k_img']
+mnist_test_10k_label_path = path_cfg['dataset_mnist_t10k_label']
+cifar_10_path = path_cfg['dataset_cifar_10_path']
