f387010b4ef723aecf8ee0e6b116a7e2639fe2d3
csruiliu <rui.liu@proton.me>
2020-12-26 12:17:33 -0600

    refine code [2]

--- pack/core/train_pack.py
+++ pack/core/train_pack.py
@@ -7,9 +7,8 @@ import os
 import pack.config.config_parameter as cfg_para
 import pack.config.config_path as cfg_path
 from pack.models.model_importer import ModelImporter
-from pack.tools.utils_img_func import load_imagenet_raw, load_imagenet_labels_onehot
-from pack.tools.utils_img_func import load_cifar10_keras
-from pack.tools.utils_img_func import load_mnist_image, load_mnist_label_onehot
+from pack.tools.dataset_loader import data_loader
+from pack.tools.img_tool import load_imagenet_raw
 
 
 def build_model_pack():
@@ -197,51 +196,16 @@ if __name__ == '__main__':
     # Hyperparameters due to dataset
     #################################################
 
-    img_width = 0
-    img_height = 0
-    num_class = 0
-    num_channel = 0
+    args_list = data_loader(train_dataset)
 
-    if train_dataset == 'imagenet':
-        train_img_path = cfg_path.imagenet_t50k_img_raw_path
-        train_label_path = cfg_path.imagenet_t50k_label_path
-        test_img_path = cfg_path.imagenet_t1k_img_raw_path
-        test_label_path = cfg_path.imagenet_t1k_label_path
-
-        img_width = cfg_para.img_width_imagenet
-        img_height = cfg_para.img_height_imagenet
-        num_channel = cfg_para.num_channels_rgb
-        num_class = cfg_para.num_class_imagenet
-
-        train_label = load_imagenet_labels_onehot(train_label_path, num_class)
-        test_label = load_imagenet_labels_onehot(test_label_path, num_class)
-
-    elif train_dataset == 'cifar10':
-        img_width = cfg_para.img_width_cifar10
-        img_height = cfg_para.img_height_cifar10
-        num_channel = cfg_para.num_channels_rgb
-        num_class = cfg_para.num_class_cifar10
-
-        train_feature, train_label, test_feature, test_label = load_cifar10_keras()
-
-    elif train_dataset == 'mnist':
-        img_width = cfg_para.img_width_mnist
-        img_height = cfg_para.img_height_mnist
-        num_channel = cfg_para.num_channels_bw
-        num_class = cfg_para.num_class_mnist
-
-        train_img_path = cfg_path.mnist_train_img_path
-        train_label_path = cfg_path.mnist_train_label_path
-        test_img_path = cfg_path.mnist_test_10k_img_path
-        test_label_path = cfg_path.mnist_test_10k_label_path
-
-        train_feature = load_mnist_image(train_img_path)
-        train_label = load_mnist_image(test_img_path)
-        test_feature = load_mnist_image(test_img_path)
-        test_label = load_mnist_label_onehot(test_label_path)
-
-    else:
-        raise ValueError('Training Dataset is invaild, only support mnist, cifar10, imagenet')
+    img_width = args_list[0]
+    img_height = args_list[1]
+    num_channel = args_list[2]
+    num_class = args_list[3]
+    train_feature = args_list[4]
+    train_label = args_list[5]
+    test_feature = args_list[6]
+    test_label = args_list[7]
 
     #########################
     # Build packed model
